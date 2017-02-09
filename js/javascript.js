19daccc1131ea886bf3af77bd18fb76b4cf3e61d
mohammed-esmail <p14138693@my365.dmu.ac.uk>
2017-02-09 17:42:37 +0000

    Added smooth scrolling functionality

--- js/javascript.js
+++ js/javascript.js
@@ -19,3 +19,27 @@ btn.addEventListener("click", OpenModal);
 
 
 }());
+
+$(document).ready(function(){
+
+  $(".button-dark").on('click', function(event) {
+
+
+    if (this.hash !== "") {
+     
+      event.preventDefault();
+
+
+      var hash = this.hash;
+
+     
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
