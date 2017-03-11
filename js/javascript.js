b2e9a03fb41268280fd5c9782d0563a5ddb09a3c
mohammed-esmail <p14138693@my365.dmu.ac.uk>
2017-03-11 02:08:00 +0000

    Added image hover effect and text on image title

--- js/javascript.js
+++ js/javascript.js
@@ -1,45 +1,57 @@
+(function () {
 
-(function(){
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the button that opens the modal
+    var image = document.getElementById("modal1");
 
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-function OpenModal() {
-modal.style.display = "block";
+    // When the user clicks the button, open the modal
+    image.onclick = function () {
+        modal.style.display = "block"; //opens modal
 
-return false;
-}
+    }
 
-btn.addEventListener("click", OpenModal);
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none"; //hides modal
+
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none"; //hides modal
+            image.style.display = "block"//shows button
+        }
+    }
 
 
 }());
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $(".button-dark").on('click', function(event) {
-
-
-    if (this.hash !== "") {
-     
-      event.preventDefault();
+    $(".button-dark").on('click', function (event) {
 
 
-      var hash = this.hash;
+        if (this.hash !== "") {
 
-     
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        
-        window.location.hash = hash;
-      });
-    } 
-  });
+            event.preventDefault();
+
+
+            var hash = this.hash;
+
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+
+                window.location.hash = hash;
+            });
+        }
+    });
 });
